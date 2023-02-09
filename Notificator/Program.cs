1aaf0432153923592701b3d8362e66048ba15722
Damian Żółkoś <damianzolkos@gmail.com>
2023-02-09 21:14:35 +0100

    Initial commit

---
+++ Notificator/Program.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Toolkit.Uwp.Notifications;
+
+namespace Notificator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var notification = new ToastContentBuilder()
+                .AddArgument("action", "viewConversation")
+                .AddArgument("conversationId", 6969);
+
+            if (args.Length > 0)
+            {
+                foreach (var arg in args)
+                {
+                    notification.AddText(arg);
+                }
+            }
+            else
+            {
+                notification.AddText("Some notification");
+            }
+
+            notification.Show();
+        }
+    }
+}
